b1220a43a5d9ead56bc61e8e17fa93b35afb09f3
yukiya272 <kybasket@outlook.jp>
2022-11-07 12:08:49 +0900

    課題1-1

--- prmn2022a/src/lecture01/Main.java
+++ prmn2022a/src/lecture01/Main.java
@@ -2,6 +2,5 @@ package lecture01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("B2210820 Yukiya Kimura");
     }
 }
---
+++ prmn2022a/src/lecture01/kadai11.java
@@ -0,0 +1,7 @@
+package lecture01;
+
+public class kadai11 {
+    public static void main(String[] args) {
+        System.out.println("B2210820 Yukiya Kimura");
+    }
+}
