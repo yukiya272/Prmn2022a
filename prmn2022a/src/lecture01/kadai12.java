8f2aa887bb896ae21973770da096b9c31fd5a78e
yukiya272 <kybasket@outlook.jp>
2022-11-07 12:14:14 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args) {
+        int studentNumber = 2210820;
+
+        System.out.println("B" + studentNumber + " Yukiya Kimura");
+    }
+}
