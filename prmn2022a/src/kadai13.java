683324e913a97e6d200d2eee858dadcbfb054e44
yukiya272 <kybasket@outlook.jp>
2022-11-07 12:20:50 +0900

    課題1-3

---
+++ prmn2022a/src/kadai13.java
@@ -0,0 +1,13 @@
+public class kadai13 {
+    public static void main(String[] args) {
+        int age =20;
+
+        if(age < 20){
+            System.out.println("I am " + age + "years old so I cannot drink liquor.");
+        }
+
+        else if(age >= 20){
+            System.out.println("I am " + age + "years old so I can drink liquor.");
+        }
+    }
+}
